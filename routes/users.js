d46f3552a89f934f0cf668979d50aac6fcbb8c17
Jacob <Yanicak>
2020-03-19 14:10:31 -0400

    updating users route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  console.log(req.body);
+  console.log(req.json);
+  res.sendStatus(200);
+});
+
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  console.log('req.body:');
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 module.exports = router;
+
